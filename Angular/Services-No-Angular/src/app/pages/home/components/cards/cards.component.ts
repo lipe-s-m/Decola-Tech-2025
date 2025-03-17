d178da04b9c02c589b8ea013f6f0f6bc6975f517
lipe-s-m <felipe.s.m1609@gmail.com>
2025-03-17 17:15:10 -0300

    :sparkles: Componentes desacoplados - Consumo de dados via Mock

--- Angular/Services-No-Angular/src/app/pages/home/components/cards/cards.component.ts
+++ Angular/Services-No-Angular/src/app/pages/home/components/cards/cards.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { listPokemons } from '../../../../data/datafake';
 
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cards.component.html',
-  styleUrl: './cards.component.css'
+  styleUrl: './cards.component.css',
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
+  public namePokemon: string = '';
+  public idPokemon: string = '';
+  public imagePokemon: string = '';
+  public atributosPokemon: string[] = [];
+  public listPokemons: any;
 
+  ngOnInit(): void {
+    this.obterListaPokemons();
+  }
+  obterListaPokemons(): void {
+    const res = listPokemons;
+    this.listPokemons = res;
+    console.log(listPokemons);
+  }
 }
