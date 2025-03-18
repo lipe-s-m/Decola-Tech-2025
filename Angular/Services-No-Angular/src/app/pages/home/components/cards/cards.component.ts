f11bc0c1b3ddd3d8fd5d3d2e3cb813f9a0d24b9b
lipe-s-m <felipe.s.m1609@gmail.com>
2025-03-18 02:53:43 -0300

    :sparkles: Obtendo dados via API

--- Angular/Services-No-Angular/src/app/pages/home/components/cards/cards.component.ts
+++ Angular/Services-No-Angular/src/app/pages/home/components/cards/cards.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { listPokemons } from '../../../../data/datafake';
+import { PokemonService } from '../../../../services/pokemon.service';
+import { PokemonData } from '../../../../models/PokemonData';
+import { PokemonDataList } from '../../../../models/pokemonDataList';
+import { SearchService } from '../../../../services/search.service';
 
 @Component({
   selector: 'app-cards',
@@ -10,18 +13,86 @@ import { listPokemons } from '../../../../data/datafake';
   styleUrl: './cards.component.css',
 })
 export class CardsComponent implements OnInit {
-  public namePokemon: string = '';
-  public idPokemon: string = '';
-  public imagePokemon: string = '';
-  public atributosPokemon: string[] = [];
-  public listPokemons: any;
+  // public namePokemon: string = '';
+  // public idPokemon: string = '';
+  // public imagePokemon: string = '';
+  // public atributosPokemon: string[] = [];
 
-  ngOnInit(): void {
-    this.obterListaPokemons();
+  public pokemon: PokemonData;
+  public listaPokemons: PokemonData[];
+  private listaRegiaoPokemons: PokemonDataList;
+
+  constructor(
+    private pokemonService: PokemonService,
+    private searchService: SearchService
+  ) {
+    this.pokemon = {
+      id: '',
+      name: 'res.name',
+      sprites: {
+        front_default: '',
+      },
+      types: [],
+    };
+    this.listaRegiaoPokemons = {
+      region: '',
+      quantidadePokemons: 0,
+      pokemon_entries: [],
+    };
+    this.listaPokemons = [];
   }
-  obterListaPokemons(): void {
-    const res = listPokemons;
-    this.listPokemons = res;
-    console.log(listPokemons);
+  ngOnInit(): void {
+    this.getAllPokemonsFromRegion('kanto');
+    this.searchService.inputSearch$.subscribe((search) => {
+      if (search) {
+        this.getPokemonByName(search);
+      }
+    });
+    this.searchService.regionSearch$.subscribe((region) => {
+      if (region) {
+        this.getAllPokemonsFromRegion(region);
+      }
+    });
+  }
+
+  getPokemonByName(namePokemon: string): void {
+    this.pokemonService.getPokemonByName(namePokemon).subscribe({
+      next: (res) => {
+        this.pokemon = {
+          id: res.id,
+          name: res.name,
+          sprites: res.sprites,
+          types: res.types,
+        };
+        this.listaPokemons.push(this.pokemon);
+        this.listaPokemons.sort((a, b) => Number(a.id) - Number(b.id));
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+  getAllPokemonsFromRegion(regionPokemon: string): void {
+    this.listaPokemons = [];
+
+    this.pokemonService.getAllPokemonsFromRegion(regionPokemon).subscribe({
+      next: (pokemon) => {
+        this.listaRegiaoPokemons = {
+          region: regionPokemon,
+          quantidadePokemons: 20,
+          pokemon_entries: pokemon.pokemon_entries,
+        };
+
+        let i = 0;
+        this.listaRegiaoPokemons.pokemon_entries.forEach((poke) => {
+          ++i;
+          if (i >= 20) return;
+          this.getPokemonByName(poke.pokemon_species.name);
+        });
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
